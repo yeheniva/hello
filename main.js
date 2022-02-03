f8f6f1e807570f48a48e44db0e88a517ee505b1c
Yevhen <yevheniva0689@gmail.com>
2022-02-03 19:17:46 +0200

    main.js

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
-function checkAge(age) {
-    if (age > 18) {
-        return true;
+function min(a, b) {
+    if (a < b) {
+        return a;
     } else {
-        return confirm('Its ok');
+        return b;
     }
 }
 
-function checkAge(age) {
-    return (age > 18) || confirm('Its ok?');
+function min(a, b) {
+    return a < b ? a : b;
 }
 
-function checkAge(age) {
-    return (age > 18) ? true : confirm('Its ok?');
+function pow(x, n) {
+    let result = x;
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+    alert( pow(x, n) );
 }
